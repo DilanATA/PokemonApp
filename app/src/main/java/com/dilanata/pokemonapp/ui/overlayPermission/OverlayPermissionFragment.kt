2e612b63fcc949c99af2fac4f620bff3dcb1c2eb
Dilan <dilan.ata@pinsoft.ist>
2022-07-06 00:40:12 +0300

    permission and foregroundservice added

---
+++ app/src/main/java/com/dilanata/pokemonapp/ui/overlayPermission/OverlayPermissionFragment.kt
@@ -0,0 +1,66 @@
+package com.dilanata.pokemonapp.ui.overlayPermission
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startForegroundService
+import androidx.fragment.app.Fragment
+import com.dilanata.pokemonapp.R
+import com.dilanata.pokemonapp.base.BaseFragment
+import com.dilanata.pokemonapp.databinding.FragmentOverlayPermissionBinding
+import com.dilanata.pokemonapp.service.ForegroundService
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+
+@Suppress("COMPATIBILITY_WARNING")
+@ExperimentalCoroutinesApi
+@FlowPreview
+@AndroidEntryPoint
+class OverlayPermissionFragment : BaseFragment<FragmentOverlayPermissionBinding>(R.layout.fragment_overlay_permission) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkOverlayPermission()
+        startService()
+    }
+    // method for starting the service
+    fun startService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // check if the user has already granted
+            // the Draw over other apps permission
+            if (Settings.canDrawOverlays(requireContext())) {
+                // start the service based on the android version
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(requireContext(), Intent(requireContext(), ForegroundService::class.java))
+                } else {
+                    startService()
+                }
+            }
+        } else {
+            startService()
+        }
+    }
+    // method to ask user to grant the Overlay permission
+    fun checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(requireContext())) {
+                // send user to the device settings
+                val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                startActivity(myIntent)
+            }
+        }
+    }
+
+    // check for permission again when user grants it from
+    // the device settings, and start the service
+    override fun onResume() {
+        super.onResume()
+        startService()
+    }
+
+}
